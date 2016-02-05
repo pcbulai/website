0504f983fb5fcb7aa5dc12826008aa44ec1880cb
Paul Bulai <paul.cbulai@gmail.com>
2016-02-05 12:05:55 +0200

    major updates and bugfixes

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -1,30 +1,12 @@
-/**
- *
- *  Web Starter Kit
- *  Copyright 2014 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
-(function () {
-  'use strict';
+(function() {
 
-  var querySelector = document.querySelector.bind(document);
+  var $ = document.querySelector.bind(document);
 
-  var navdrawerContainer = querySelector('.navdrawer-container');
-  var appbarElement = querySelector('.app-bar');
-  var menuBtn = querySelector('.menu');
-  var main = querySelector('main');
+  var navdrawerContainer = $('.navdrawer-container');
+  var appbarElement = $('.app-bar');
+  var menuBtn = $('.menu');
+  var main = $('main');
+  var socialLinks = $('.social-items');
 
   function closeMenu() {
     appbarElement.classList.remove('open');
@@ -36,11 +18,33 @@
     navdrawerContainer.classList.toggle('open');
   }
 
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  function mpEventName(input) {
+    return capitalizeFirstLetter(input.target.href.split('#')[1]);
+  }
+
   main.addEventListener('click', closeMenu);
   menuBtn.addEventListener('click', toggleMenu);
-  navdrawerContainer.addEventListener('click', function (event) {
+  navdrawerContainer.addEventListener('click', function(event) {
     if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
+      mixpanel.track('Click', {
+        'Section': mpEventName(event)
+      });
       closeMenu();
+    }
+  });
+  socialLinks.addEventListener('click', function(event) {
+    if (event.target.nodeName === 'A' || event.target.nodeName === 'I') {
+      if (event.target.id === 'email') {
+        mixpanel.track('Clicked on email');
+      } else {
+        mixpanel.track('Socializing', {
+          'Social profile': capitalizeFirstLetter(event.target.id)
+        });
+      }
     }
   });
 })();
